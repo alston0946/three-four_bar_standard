52c3339a0c79b6c6e3035a9b3f5504390f0a9ed7
alston0946 <1281930836@qq.com>
2026-05-14 15:34:49 +0800

    Update github_entry.py

--- automation/github_entry.py
+++ automation/github_entry.py
@@ -72,7 +72,7 @@ def main():
     # -------------------------
     script_name = os.getenv(
         "SCAN_SCRIPT_PATH",
-        ""three_bar_play_multi_dates.py""
+        "three_bar_play_multi_dates.py"
     )
 
     script_path = REPO_ROOT / script_name
